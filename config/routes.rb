7768f55814dd6f12c2f65923abcf371698204630
Donard97 <donardgola@gmail.com>
2022-01-11 19:38:29 +0100

    Add content to the splash sreen page

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  root to: 'users#index'
 end
