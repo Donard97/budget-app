daab1ecc48563b1ca434e7aee03b38c697f16a58
Donard97 <donardgola@gmail.com>
2022-01-12 11:18:42 +0100

    Update roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get 'payments/index'
-  get 'payments/show'
-  get 'groups/index'
-  get 'groups/show'
-  get 'users/index'
-  get 'users/show'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-
+  
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
   root to: 'users#index'
+
+  resources :groups
 end
